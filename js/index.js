e2a1e54063d14964be3f25cd424193604c4314bf
Hallowgate <wjddbscjf0@gmail.com>
2025-11-20 21:00:54 +0900

    2025/11/20

--- js/index.js
+++ js/index.js
@@ -3,6 +3,8 @@ $(document).ready(function(){
     eventBind();
 });
 
+var count = 5;
+
 function start() {
     $(".main_screen").load("html/ladder.html");
     $(".tit_name").text("사다리타기");
@@ -24,4 +26,19 @@ function eventBind() {
         $(".main_screen").load("html/dice.html");
     });
 
+    // $(".count_down_btn").click(function() {
+    // });
+
+    // $(".count_up_btn").click(function() {
+    // });
+
+
+    // 세로 막대 위의 텍스트 상자에는 기본적으로 placeholder를 통해 숫자를 입력해두고 그대로 진행 할 경우는 숫자가 value로 변경
+    // 세로 막대 밑의 텍스트 상자에는 기본적으로는 value 값이 없음 (미입력시 : 결과 때 이어진 막대의 value를 그대로 입력해 줌)
+    // 세로 막대 밑의 텍스트 상자에는 당첨자 수를 체크해서 넣어주는 시스템 추가 (나중에 추가)
+    // 사다리의 세로 막대의 갯수는 10개까지로 지정
+    // 사다리 세로 막대는 사용자가 설정한 갯수 만큼 생성
+    // 사다리 가로 선 총 갯수 (세로막대 수 *5) +-1
+    // 가로 선은 같은 위치에 그어질 수 없음
+    // 두 막대 사이의 가로 선의 갯수는 최종적으로 4개~8개 사이로만 가능
 };
